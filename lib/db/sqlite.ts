5ea9a0fb8390e8ad3ba0d1154b22ee1a030c5e66
Eugenio Doñaque <eugenio@donaque.xyz>
2026-07-24 22:36:45 -0400

    Fix db file name

--- lib/db/sqlite.ts
+++ lib/db/sqlite.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/bun-sqlite';
 import path from 'path';
 import * as schema from './schema';
 
-const dbPath = path.resolve(process.env.SQLITE_PATH ?? './', 'data', 'exporter.dh');
+const dbPath = path.resolve(process.env.SQLITE_PATH ?? './', 'data', 'exporter.db');
 
 // Singleton prevents multiple connections during Next.js hot reload in dev
 const globalForDb = global as typeof global & { _sqlite?: Database };
